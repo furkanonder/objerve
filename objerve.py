80fae404cd1d7b1034f546ef2d4e75bdf0ba2814
furkanonder <furkanonder@protonmail.com>
2022-07-24 01:35:40 +0300

    Add dynamic trace limit

--- objerve.py
+++ objerve.py
@@ -3,9 +3,10 @@ from collections import defaultdict
 
 
 class Descriptor:
-    def __init__(self, attribute, hooks):
+    def __init__(self, attribute, hooks, trace_limit):
         self.attribute = f"_{attribute}"
         self.hooks = hooks
+        self.trace_limit = trace_limit
 
     def __set__(self, instance, value):
         if "set" in self.hooks:
@@ -26,18 +27,21 @@ class Descriptor:
         delattr(instance, self.attribute)
 
     def print_stack(self):
-        summary, *_ = traceback.extract_stack(limit=3)
+        summary, *_ = traceback.extract_stack(limit=self.trace_limit)
         print(*traceback.format_list([summary]))
 
 
 def watch(**kwargs):
     attrs = defaultdict(list)
+    trace_limit = kwargs.pop("trace_limit", 3)
+
     for hook in kwargs:
         for attr in kwargs[hook]:
             attrs[attr].append(hook)
 
-    def decorator(cls):
+    def inner(cls):
         for attr in attrs:
-            setattr(cls, attr, Descriptor(attr, attrs[attr]))
+            setattr(cls, attr, Descriptor(attr, attrs[attr], trace_limit))
         return cls
-    return decorator
+
+    return inner

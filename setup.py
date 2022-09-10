d3a428a919d9634015b9bb2a234c1a502d979ad1
furkanonder <furkanonder@protonmail.com>
2022-09-10 17:12:39 +0300

    Set version as 0.2.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def get_long_description():
 
 setup(
     name="objerve",
-    version="0.1.0",
+    version="0.2.0",
     long_description=get_long_description(),
     long_description_content_type="text/markdown",
     description="Tiny observer for the attributes of Python objects.",
